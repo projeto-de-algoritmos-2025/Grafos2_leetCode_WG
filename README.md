9c5513b0b48474231106289149f988d60793fa60
Wildemberg <junior_sales2010@hotmail.com>
2025-09-21 00:42:43 -0300

    Documento criado e primeira questão repondida

--- README.md
+++ README.md
@@ -12,41 +12,41 @@
 
 ## Sobre
 
-Esse exercício visa buscar a resolução dos exercícios escolhidos pela plataforma do LeetCode, com o objetivo de mostrar nossos conhecimentos aprendidos no conteúdo de grafos 1. Neles vamos aplicar conceitos e conhecimentos de lógica aprendidos na matéria de Projeto de Algoritmos com o professor Maurício Serrano.
+Esse exercício visa buscar a resolução dos exercícios escolhidos pela plataforma do LeetCode, com o objetivo de mostrar nossos conhecimentos aprendidos no conteúdo de grafos 2. Neles vamos aplicar conceitos e conhecimentos de lógica aprendidos na matéria de Projeto de Algoritmos com o professor Maurício Serrano.
 
 Principais funcionalidades:
-- Descobrir o maior caminho dentro de uma árvore.
-- Identificar a existências de ciclos e pais duplicados para um mesmo nó em um grafo direcionado.
-- Identificação de ciclos dentro de uma implementação de uma matriz de adjacência.
+- Colocar as funcionalidades
 
 
 ## Screenshots
 
-### Visualização de Grafo → Execução de Algoritmo →  Resultado de Caminho Mínimo
+### 
 
 #### Questão 1   
 
-![Programação do curso](./imgs/questao_1.png)
+[Network Delay Time](/imgs/questao_1.png)
+
+[Link](https://leetcode.com/problems/network-delay-time/description/?envType=problem-list-v2&envId=shortest-path)
 
 #### Questão 1 no LeetCode  
 
-![Programação do curso](./imgs/questao_1_resp.png)
+[Resposta questão 1](/imgs/resp_questao1.png)
 
 ---
 
 #### Questão 2   
 
-![Execução de Algoritmo](./imgs/questao_2.png)
+[Minimum Obstacle Removal to Reach Corner](/imgs/questao_2.png)
 
-**Explicação da Resolução:** Foi utilizado dois vetores auxiliares para marcação de quem são filhos e quem são pais dentro das arestas passadas, com isso seria possível verificar se algum filho estava apontando para algum pai (Ciclo) ou se algum filho estava recebendo um segundo pai (Quebra da árvore).
+[Link](https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/description/?envType=problem-list-v2&envId=shortest-path)
+
+#### Questão 2 no LeetCode  
 
 ---
 
 #### Questão 3   
 
-![Resultado de Caminho Mínimo](./imgs/questao_3.png)
 
-**Explicação da Resolução:** Para esta questão foi utilizado uma lógica de matriz de adjacencia que guardava todas as dependências fornecidas. Com isso foi desenvolvido um modelo de DFS para percorrer a matriz com ajuda de um vetor de visitados, para analisar se em algum momento as dependências de cursos criavam um ciclo.
 
 ---
 
@@ -60,7 +60,7 @@ gcc questao_x.c -o questao_x && ./questao_x
 
 ## Apresentação
 
-Vídeo disponível em: [youtube](https://youtu.be/K3ExD20ChGY)
+Vídeo disponível em: [youtube]()
 
 ## Outros
 
--- code/questao1.c
+++ code/questao1.c
@@ -1,32 +1,118 @@
-/**
-// Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
+typedef struct {
+    int para;
+    int peso;
+} Aresta;
 
-static int max(int a, int b){
-    return a>b ? a:b;
-}
-static int dfs(struct TreeNode* node, int *ans){
-    if(!node) return 0;
-    int leftGain = dfs(node->left, ans);
-    int rightGain = dfs(node->right, ans);
-    if(leftGain<0)
-        leftGain=0;
-    if(rightGain<0)
-        rightGain=0;
-    int betterNode = node->val + leftGain + rightGain;
-    if (betterNode > *ans) 
-        *ans = betterNode;
-    return node->val + max(leftGain, rightGain);
-}
+typedef struct {
+    Aresta *itens;
+    int tamanho;
+    int capacidade;
+} ListaArestas;
 
-int maxPathSum(struct TreeNode* root) {
-    int ans = INT_MIN;
-    dfs(root, &ans);
-    return ans;
+static void listaArestasInit(ListaArestas *lista, int capacidade) {
+    if (capacidade < 4) capacidade = 4;
+    lista->itens = (Aresta*)malloc(sizeof(Aresta) * capacidade);
+    lista->tamanho = 0;
+    lista->capacidade = capacidade;
+}
+static void listaArestasPush(ListaArestas *lista, Aresta a) {
+    if (lista->tamanho == lista->capacidade) {
+        lista->capacidade *= 2;
+        lista->itens = (Aresta*)realloc(lista->itens, sizeof(Aresta) * lista->capacidade);
+    }
+    lista->itens[lista->tamanho++] = a;
 }
 
+typedef struct {
+    int no;
+    int dist;
+} ItemFila;
+
+typedef struct {
+    ItemFila *dados;   /* 1-indexado */
+    int tamanho;
+    int capacidade;
+} HeapMin;
+
+static void heapTrocar(ItemFila *a, ItemFila *b) { ItemFila t = *a; *a = *b; *b = t; }
+static void heapInit(HeapMin *h, int capacidade) {
+    if (capacidade < 8) capacidade = 8;
+    h->dados = (ItemFila*)malloc(sizeof(ItemFila) * (capacidade + 1));
+    h->tamanho = 0;
+    h->capacidade = capacidade + 1;
+}
+static void heapPush(HeapMin *h, ItemFila it) {
+    if (h->tamanho + 1 >= h->capacidade) {
+        h->capacidade *= 2;
+        h->dados = (ItemFila*)realloc(h->dados, sizeof(ItemFila) * h->capacidade);
+    }
+    int i = ++h->tamanho;
+    h->dados[i] = it;
+    while (i > 1 && h->dados[i].dist < h->dados[i/2].dist) {
+        heapTrocar(&h->dados[i], &h->dados[i/2]);
+        i /= 2;
+    }
+}
+static int heapVazio(HeapMin *h) { return h->tamanho == 0; }
+static ItemFila heapPop(HeapMin *h) {
+    ItemFila topo = h->dados[1];
+    h->dados[1] = h->dados[h->tamanho--];
+    int i = 1;
+    while (1) {
+        int l = i*2, r = l+1, m = i;
+        if (l <= h->tamanho && h->dados[l].dist < h->dados[m].dist) m = l;
+        if (r <= h->tamanho && h->dados[r].dist < h->dados[m].dist) m = r;
+        if (m == i) break;
+        heapTrocar(&h->dados[i], &h->dados[m]);
+        i = m;
+    }
+    return topo;
+}
+
+int networkDelayTime(int** times, int timesSize, int* timesColSize, int n, int k) {
+    (void)timesColSize;
+
+    ListaArestas *grafo = (ListaArestas*)malloc((n + 1) * sizeof(ListaArestas));
+    for (int i = 1; i <= n; ++i) listaArestasInit(&grafo[i], 2);
+    for (int i = 0; i < timesSize; ++i) {
+        int de = times[i][0], para = times[i][1], peso = times[i][2];
+        listaArestasPush(&grafo[de], (Aresta){para, peso});
+    }
+
+    const int INF = INT_MAX / 4;
+    int *distancias = (int*)malloc((n + 1) * sizeof(int));
+    for (int i = 1; i <= n; ++i) distancias[i] = INF;
+    distancias[k] = 0;
+
+    HeapMin fila;
+    heapInit(&fila, n + 5);
+    heapPush(&fila, (ItemFila){k, 0});
+
+    while (!heapVazio(&fila)) {
+        ItemFila atual = heapPop(&fila);
+        int no = atual.no, d = atual.dist;
+        if (d > distancias[no]) continue;
+
+        for (int i = 0; i < grafo[no].tamanho; ++i) {
+            int prox = grafo[no].itens[i].para;
+            int w = grafo[no].itens[i].peso;
+            if (d + w < distancias[prox]) {
+                distancias[prox] = d + w;
+                heapPush(&fila, (ItemFila){prox, distancias[prox]});
+            }
+        }
+    }
+
+    int resposta = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (distancias[i] == INF) { resposta = -1; break; }
+        if (distancias[i] > resposta) resposta = distancias[i];
+    }
+
+    for (int i = 1; i <= n; ++i) free(grafo[i].itens);
+    free(grafo);
+    free(distancias);
+    free(fila.dados);
+
+    return resposta;
+}
